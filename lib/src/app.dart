b7c98591e9d265d8106bbbc32f8225792ad10f5c
Saulo d'Souza <saulosouza@proton.me>
2024-02-27 22:32:47 -0300

    migrate to GoRouter

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -13,6 +13,8 @@ class MyApp extends StatelessWidget {
       restorationScopeId: 'app',
       onGenerateTitle: (BuildContext context) => 'My Shop'.hardcoded,
       theme: ThemeData(
+        // * Use this to toggle Material 3 (defaults to true since Flutter 3.16)
+        useMaterial3: true,
         primarySwatch: Colors.grey,
         appBarTheme: const AppBarTheme(
           backgroundColor: Colors.black87,
--- lib/src/common_widgets/alert_dialogs.dart
+++ lib/src/common_widgets/alert_dialogs.dart
@@ -14,42 +14,38 @@ Future<bool?> showAlertDialog({
   String? cancelActionText,
   String defaultActionText = 'OK',
 }) async {
-  if (kIsWeb || !Platform.isIOS) {
-    return showDialog(
-      context: context,
-      builder: (context) => AlertDialog(
-        title: Text(title),
-        content: content != null ? Text(content) : null,
-        actions: <Widget>[
-          if (cancelActionText != null)
-            TextButton(
-              child: Text(cancelActionText),
-              onPressed: () => context.pop(false),
-            ),
-          TextButton(
-            child: Text(defaultActionText),
-            onPressed: () => context.pop(true),
-          ),
-        ],
-      ),
-    );
-  }
-  return showCupertinoDialog(
+  return showDialog(
     context: context,
-    builder: (context) => CupertinoAlertDialog(
+    // * Only make the dialog dismissible if there is a cancel button
+    barrierDismissible: cancelActionText != null,
+    // * AlertDialog.adaptive was added in Flutter 3.13
+    builder: (context) => AlertDialog.adaptive(
       title: Text(title),
       content: content != null ? Text(content) : null,
-      actions: <Widget>[
-        if (cancelActionText != null)
-          CupertinoDialogAction(
-            child: Text(cancelActionText),
-            onPressed: () => context.pop(false),
-          ),
-        CupertinoDialogAction(
-          child: Text(defaultActionText),
-          onPressed: () => context.pop(true),
-        ),
-      ],
+      // * Use [TextButton] or [CupertinoDialogAction] depending on the platform
+      actions: kIsWeb || !Platform.isIOS
+          ? <Widget>[
+              if (cancelActionText != null)
+                TextButton(
+                  child: Text(cancelActionText),
+                  onPressed: () => context.pop(false),
+                ),
+              TextButton(
+                child: Text(defaultActionText),
+                onPressed: () => context.pop(true),
+              ),
+            ]
+          : <Widget>[
+              if (cancelActionText != null)
+                CupertinoDialogAction(
+                  child: Text(cancelActionText),
+                  onPressed: () => context.pop(false),
+                ),
+              CupertinoDialogAction(
+                child: Text(defaultActionText),
+                onPressed: () => context.pop(true),
+              ),
+            ],
     ),
   );
 }
